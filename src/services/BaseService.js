bdf24dbfd31e227b16ced201cc56fe929b7f8d79
“Giovanni” <giovannitakeshisesta@gmail.com>
2022-06-12 21:18:18 +0200

    baseUrl

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const createHttp = () => {
   const http = axios.create({
-    baseURL: "http://localhost:3002/api",
+    // baseURL: "http://localhost:3002/api",
+    baseURL: `https://draganddrop-giovanni.onrender.com/api`
   });
 
   return http;
